b8fd5ee5c84fa915cf37f240bb876fae68f669b6
aditya2604 <aditya26042005@gmail.com>
2022-06-16 03:18:16 -0700

    add API for direct access from chromedriver_autoinstall

--- chromedriver_autoinstall/__init__.py
+++ chromedriver_autoinstall/__init__.py
@@ -5,3 +5,9 @@ def install():
    script.install()
    path = os.path.abspath(os.getcwd())
    print(f"install path: {path}")
+
+def get_platform():
+   return script.get_platform()
+
+def get_version():
+   return script.get_version()
